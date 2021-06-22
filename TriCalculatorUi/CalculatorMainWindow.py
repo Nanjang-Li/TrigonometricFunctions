424d93e71282d77fb28e4b255a2677a0f0f4ccee
lxk1203 <604539862@qq.com>
2021-06-22 22:04:38 +0800

    TriCalculatorUi

--- TriCalculatorUi/CalculatorMainWindow.py
+++ TriCalculatorUi/CalculatorMainWindow.py
@@ -19,14 +19,51 @@ class MainWindow(QMainWindow):
         self.ui.number_7_button.clicked.connect(lambda: self.display_number(7))
         self.ui.number_8_button.clicked.connect(lambda: self.display_number(8))
         self.ui.number_9_button.clicked.connect(lambda: self.display_number(9))
+        self.ui.del_button.clicked.connect(self.display_delete_one_number)
+        self.ui.reset_button.clicked.connect(self.display_reset)
+        self.ui.dot_button.clicked.connect(self.display_dot)
 
     def display_number(self, number):
+        """
+        在显示框上显示按钮对应的值
+        :param number: 按钮对应的值
+        :return: None
+        """
         display_content = self.ui.display_box.text()  # 获取显示框的文本
         if display_content == "0":
-            display_content = str(number)   # 当前内容为0，直接更新为的按钮数字
+            display_content = str(number)  # 当前内容为0，直接更新为的按钮数字
         else:
             display_content += str(number)  # 当前内容不为0，追加数字
-        self.ui.display_box.setText(display_content)
+        self.ui.display_box.setText(display_content)    # 回显内容
+
+    def display_dot(self):
+        """
+        在显示框上显示一个小数点.
+        :return: None
+        """
+        display_content = self.ui.display_box.text()  # 获取显示框的文本
+        display_content += "."  # 追加一个小数点
+        self.ui.display_box.setText(display_content)    # 回显内容
+
+    def display_delete_one_number(self):
+        """
+        清除当前显示框上数值最右侧的一位数；
+        当显示框上的数值只有一位时，再次按清除按钮显示0；
+        :return: None;
+        """
+        display_content = self.ui.display_box.text()  # 获取显示框文本
+        if len(display_content) == 1:
+            display_content = "0"  # 当显示框上的数值只有一位时，再次按清除按钮显示0；
+        else:
+            display_content = display_content[:-1]  # 清除当前显示框上数值最右侧的一位数
+        self.ui.display_box.setText(display_content)    # 回显内容
+
+    def display_reset(self):
+        """
+        重置显示框上的内容为0
+        :return:None
+        """
+        self.ui.display_box.setText("0")
 
 
 if __name__ == "__main__":
