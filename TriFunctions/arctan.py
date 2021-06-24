0be49f6950e9173565c2fbec6d97f7001055c7a2
zouxuan <893729040@qq.com>
2021-06-24 09:42:26 +0800

    add arctan function

---
+++ TriFunctions/arctan.py
@@ -0,0 +1,30 @@
+from math import pi
+from math import fabs
+
+def atan(x):
+    """
+    :param x: input tan value of angle
+    :return:
+    """
+    if fabs(x) < 1:      # 输入值绝对值小于1时，采用泰勒级数展开
+        g = x
+        t = x
+        n = 1
+        while (n < 9999):
+            t = ((-1) ** n) * (x ** (2 * n + 1)) / (2 * n + 1)
+            g += t
+            n += 1
+    else:               # 输入值绝对值大于1，用 pi/2 - atan(x)
+        x = 1/x
+        g = x
+        t = x
+        n = 1
+        while (n < 9999):
+            t = ((-1) ** n) * (x ** (2 * n + 1)) / (2 * n + 1)
+            g += t
+            n += 1
+        g = pi/2 - g
+
+    g = round(g / pi * 180, 2)
+    return g
+
