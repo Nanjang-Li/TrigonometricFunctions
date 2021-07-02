e07a2b7343e6527610b8d0a09fa85720f18299c3
Nanjang-Li <67055528+Nanjang-Li@users.noreply.github.com>
2021-07-02 23:24:15 +0800

    Update utils.py
    角度弧度值转换函数

--- TriFunctions/utils.py
+++ TriFunctions/utils.py
@@ -1,9 +1,12 @@
 from math import pi
 
+
 def deg2rad(x):
     '''
+    
     :param x: 输入参数为角度值
     :return x :输出为弧度数值
+    
     '''
     x = x/180
     x = x*pi
@@ -11,8 +14,10 @@ def deg2rad(x):
 
 def rad2deg(x):
     '''
+    
      :param x: 输入参数为弧度数值
      :return x :输出为角度值
+     
     '''
     x = x/pi
     x = x*180
